9775eb9a0694dce3d2ec93591cbbb1053805db30
Qudus Amidu <qudusamidu14@gmail.com>
2023-03-11 14:48:49 -0800

    Tsk0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,6 +2,21 @@
 
 from add_0 import add
 
+
+def main():
+    """My main function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        0 for success
+    """
+
+
 a = 1
 b = 2
 print("{} + {} = {}".format(a, b, add(a, b)))
+
+main()
