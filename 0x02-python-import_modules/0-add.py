1894b0f6df57c9fd7313017b56b367dd085c172c
Qudus Amidu <qudusamidu14@gmail.com>
2023-03-12 23:27:47 -0700

    tsk0 done

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if _name_ == "_ main_":
+    from add_0 impport add
+    """My main function
+
+    Args:
+        a: first integer
+        b: second integr
+    """
+
+
+a = 1
+b = 2
+print("{0} + {1} = {2}".format(a, b, add(a, b)))
