513fcd72a150567812efe1639272a9a95d0d2f30
Qudus Amidu <qudusamidu14@gmail.com>
2023-03-13 21:27:03 -0700

    tsk4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """function that replaces an element in a list at a specific position
+
+    Args:
+    my_list: list of integers
+    idx: position of integer
+    element: new value of element at idx
+
+    Return: new list on success, else old list
+    """
+
+    i = 0
+
+    for i in my_list:
+        continue
+    if idx < 0:
+        return my_list
+    elif idx > i + 1:
+        return my_list
+    else:
+        my_list[idx] = element
