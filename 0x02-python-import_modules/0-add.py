122243560c0b7321f990a36a6a4cebb51fea083c
Qudus Amidu <qudusamidu14@gmail.com>
2023-03-14 18:21:27 -0700

    Task_0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """ prints the sum of two numbers
     """
-from add_0 import add
-a = 1
-b = 2
-""" To print 1 + 2 = 3 """
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    from add_0 import add
+    a = 1
+    b = 2
+    """ To print 1 + 2 = 3 """
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
