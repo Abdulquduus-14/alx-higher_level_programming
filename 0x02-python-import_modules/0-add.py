e1058cf80e37053eb481c5aeb4fcb0db98a031b1
Qudus Amidu <qudusamidu14@gmail.com>
2023-03-14 18:11:05 -0700

    task-0 started

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 
-if _name_ == "_ main_":
-    from add_0 impport add
-    """My main function
-
-    Args:
-        a: first integer
-        b: second integr
+if __name__ = "__main__":
+    """ prints the sum of two numbers
     """
-
-
+from add_0 import add
 a = 1
 b = 2
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+""" To print 1 + 2 = 3 """
+print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -6,7 +6,7 @@ def add(a, b):
     a: first integer
     b: second integer
 
-    Returns:
+    Return:
     The return value. a + b
     """
     return (a + b)
