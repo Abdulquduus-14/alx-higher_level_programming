19f2fa5eea955398d626fb36965a8cd23fc49e37
Qudus Amidu <qudusamidu14@gmail.com>
2023-03-14 18:54:10 -0700

    Task_1 checked

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub, mul, div
-
 if __name__ == "__main__":
-    """My main function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        0 for success
     """
+    Does some basic arithmetics
+    """
+    from calculator_1 import add, sub, mul, div
+
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
