0bb81de0c2b7e1fefad21f02be4e777f7d5b60f9
Amidu Qudus <qudusamidu14@gmail.com>
2023-03-27 16:13:47 -0700

    task3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -11,8 +11,8 @@ def safe_print_division(a, b):
     """
     try:
         result = a / b
-    except ZeroDivisionError:
-        return "None"
+    except (ZeroDivisionError, TypeError, ValueError):
+        result = "None"
     finally:
-        print("{}{}".format("Inside result:", result))
+        print("Inside result: {:d}".format(result))
         return result
