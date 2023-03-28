840463930b907bbeb65533bc6e38247e88df64b9
Amidu Qudus <qudusamidu14@gmail.com>
2023-03-28 11:41:04 -0700

    tsk0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """ an empty class Square that defines a square
+
+    Args:
+    No attributes defined
+
+    Returns: nothing
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """ an class Square that defines a square.
+    Instantiation with size attribute is done
+
+    Args:
+    size: private instance attributes defined
+
+    Returns: nothing
+    """
+    def __init__(self, my_size):
+        self.size = my_size
