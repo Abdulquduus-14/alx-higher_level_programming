1a3df02e171608eac38759eb496da942aedfbf22
Amidu Qudus <qudusamidu14@gmail.com>
2023-04-06 21:24:40 -0700

    task 1 checked

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
         Args:
         value (int): wideness of the rect
         """
-        if not value >= 0:
+        if type(value) not int:
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
@@ -41,7 +41,7 @@ class Rectangle:
         Args:
         value (int): wideness of the rect
         """
-        if not value >= 0:
+        if type(value) not int:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
