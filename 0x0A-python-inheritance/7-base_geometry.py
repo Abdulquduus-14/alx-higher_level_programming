660132de4cabf224a5260ed238b532327c677e23
Amidu Qudus <qudusamidu14@gmail.com>
2023-04-13 18:00:40 -0700

    tsk7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" an empty class BaseGeometry with raised unimplemented
+area method exception """
+
+
+class BaseGeometry:
+    """ future class representation """
+    def area(self):
+        """ area of geometry
+        Args: none
+        Returns: None
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ function that that validates value
+
+        Args:
+        name(str): name of geometery
+        value(int): size of geometry
+
+        Returns:
+        None
+        """
+        if not(isinstance(value, int)):
+            raise TypeError("{} must be an integer".format(self.__name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.__name))
