8a73bd6fddf6904b5b5d1ce594f818273956f9cf
Amidu Qudus <qudusamidu14@gmail.com>
2023-04-15 02:59:26 -0700

    tsk4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" an class Square that defines a square with size"""
+
+
+class Square:
+    """ an class Square that defines a square.
+    Instantiation with size attribute is done
+
+    Attributes
+    size (int): private instance attributes defin
+    """
+    def size(self, value):
+        """ initializes object attributs
+
+        Args:
+        size (int): length of square
+
+        Returns:
+        nothing
+        """
+        if not(isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ return the area
+
+        Args: none
+
+        Returns: area of square
+        """
+        return (self.__size * self.__size)
+
+    def size(self):
+        """ getter function
+        Args: nothing
+        Returns: nothing
+        """
+        return (self.size)
+
+    def __init__(self, size=0):
+        """ Object constructor method
+
+        Attr:
+        size(int): length of square
+
+        Returns: nothing
+        """
+        self.size(size)
